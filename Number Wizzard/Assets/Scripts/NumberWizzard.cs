aec8c7d9d2d3af8b3c64515b9b8d8a9a5df544fb
Gavin Wilkinson <gavinwilkinson+github@hotmail.com>
2018-07-08 22:05:08 +0100

    Files belong to first version of project.  Includes all the setup and stuff.

---
+++ Number Wizzard/Assets/Scripts/NumberWizzard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizzard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Welcome to number wizard");
+        Debug.Log("Pick a number");
+        Debug.Log("High number is:1000");
+        Debug.Log("Low number is:1");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Unity Tutorials
+# This is code for the unnity tutorials on udemy
+# c#, unity, blender, git
